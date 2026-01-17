44b52e77e436c2abd1cf76a9281491f4d97bb6f1
solosakala <solosakala@gmail.com>
2026-01-17 17:53:00 +0200

    refactor server.js to move default GET route above server listen statement

--- server.js
+++ server.js
@@ -11,14 +11,14 @@ const app = express()
 const HOST = 'localhost'
 const PORT = 3000
 
+/* ******************************************
+ * Default GET route
+ * ***************************************** */
+app.get("/", (req, res) => {res.send("Welcome home!")})
+
 /* ***********************
 * Log statement to confirm server operation
 * *********************** */
 app.listen(PORT, () => {
 console.log(`trial app listening on ${HOST}:${PORT}`)
 })
-
-/* ******************************************
- * Default GET route
- * ***************************************** */
-app.get("/", (req, res) => {res.send("Welcome home!")})
